d5faa0f0b5ee690c7aa194625589f1e208a3e8ad
cwrichardkim93 <cwrichardkim93@gmail.com>
2014-11-01 09:44:47 -0400

    top half of card is set up

--- Roam/Card.swift
+++ Roam/Card.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 class Card: UIView {
+    let DESCRIPTION_WIDTH_RATIO:CGFloat = 0.92
+    let DESCRIPTION_HEIGHT_RATIO:CGFloat = 0.28
+    let PHOTODESCRIPTION_SPACING_RATIO:CGFloat = 0.0001
+    let DESCRIPTIONDIVIDER_SPACING_RATIO:CGFloat = 0.0001
+    
+    let CP_RATIO:CGFloat = 0.38
+    let PP_RATIO:CGFloat = 0.247
+    let PP_X_RATIO:CGFloat = 0.0552
+    let PP_Y_RATIO:CGFloat = 0.246
+    let PP_BUFF:CGFloat = 3
+
+    let TEXT_WIDTH_RATIO = 0.9
+    let TEXT_HEIGHT_RATIO = 0.8
     
     var panGestureRecognizer:UIPanGestureRecognizer?
-    var profileCircleView: UIView?
     var profileImageView: UIImageView?
     var itinImageView: UIImageView?
     var nameLabel: UILabel?
@@ -22,7 +34,40 @@ class Card: UIView {
     override init() {
        super.init()
         self.cardSetup()
+    }
+    
+    func setupPhotos() {
+        var height = CGFloat(self.frame.size.height)
+        var width = CGFloat(self.frame.size.width)
         
+        var cp_mask = UIView(frame: CGRectMake(0, 0, width, height * CP_RATIO))
+        var pp_mask = UIView(frame: CGRectMake(width * PP_X_RATIO, height * PP_Y_RATIO, height * PP_RATIO, height * PP_RATIO))
+        var pp_circle = UIView(frame: CGRectMake(pp_mask.frame.origin.x - PP_BUFF, pp_mask.frame.origin.y - PP_BUFF, pp_mask.frame.size.width + 2 * PP_BUFF, pp_mask.frame.size.width + 2 * PP_BUFF))
+        pp_circle.backgroundColor = UIColor.whiteColor()
+        pp_circle.layer.cornerRadius = pp_circle.frame.size.height/2
+        pp_mask.layer.cornerRadius = pp_mask.frame.size.height/2
+        profileImageView = UIImageView(frame: CGRectMake(0, 0, pp_mask.frame.size.width, pp_mask.frame.size.height))
+        itinImageView = UIImageView(frame: cp_mask.frame)
+        itinImageView?.contentMode = UIViewContentMode.ScaleAspectFill
+        cp_mask.addSubview(itinImageView!)
+        pp_mask.addSubview(profileImageView!)
+        cp_mask.clipsToBounds = true
+        pp_mask.clipsToBounds = true
+        
+        itinImageView?.backgroundColor = UIColor.lightGrayColor()
+        
+        nameLabel = UILabel(frame: CGRectMake((pp_circle.frame.origin.x + pp_circle.frame.size.width), (cp_mask.frame.size.height + 7.0), self.frame.size.width, 26))
+        nameLabel?.font = UIFont(name: "HelveticaNeue", size: CGFloat(20))
+        nameLabel?.textColor = UIColor(red: 0.33, green: 0.49, blue: 0.6, alpha: 1)
+        var descriptionHeight:CGFloat = self.frame.size.height * DESCRIPTION_HEIGHT_RATIO / 2;
+        var descriptionWidth:CGFloat = self.frame.size.width * DESCRIPTION_WIDTH_RATIO;
+
+        nameLabel?.text = "TESTING"
+        
+        self.addSubview(nameLabel!)
+        self.addSubview(cp_mask)
+        self.addSubview(pp_circle)
+        self.addSubview(pp_mask)
     }
     
     override init(frame: CGRect) {
@@ -40,6 +85,7 @@ class Card: UIView {
     
     override func layoutSubviews() {
         self.backgroundColor = UIColor.whiteColor()
+        self.setupPhotos()
         self.layer.cornerRadius = 8
     }
 
