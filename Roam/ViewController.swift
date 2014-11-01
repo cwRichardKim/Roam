1caaf4ec7fe698cbc5952b726ed2c1cb927b0704
cwrichardkim93 <cwrichardkim93@gmail.com>
2014-11-01 01:29:15 -0400

    clean up

--- Roam/ViewController.swift
+++ Roam/ViewController.swift
@@ -13,10 +13,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var testObject = PFObject(className: "TestObject")
-        testObject.setObject("TEST", forKey: "testKey")
-        testObject.saveInBackgroundWithBlock(nil)
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
