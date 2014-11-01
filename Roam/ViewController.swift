6302bad4d186f151b95986a88d92fbbe885a977a
cwrichardkim93 <cwrichardkim93@gmail.com>
2014-11-01 00:27:33 -0400

    new swift project first commit

---
+++ Roam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roam
+//
+//  Created by Richard Kim on 11/1/14.
+//  Copyright (c) 2014 Richard Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
